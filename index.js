e140e4eea6d153dd2043b28d3ff10c1c4114cde3
Rose Maina <rosewairimu27@gmail.com>
2022-06-24 20:17:26 +0300

    Save changes

--- index.js
+++ index.js
@@ -14,56 +14,30 @@ let ongoingProjects = () => {
   };
   initialize();
 
-  function getProjectData() {
-    fetch("https://fierce-forest-94320.herokuapp.com/items")
-      .then((res) => res.json())
-      .then((jsondata) => {
-        let firstItem = document.querySelector("ProjectName");
-        firstItem.innerText = jsondata.projectName;
-        let secondItem = document.querySelector("ProgressAsOfDate");
-        secondItem.innerText = jsondata.progressAsOfDate;
-        let thirdItem = document.querySelector("PublicationStatus");
-        thirdItem.innerText = jsondata.publicationStatus;
-        let fourthItem = document.querySelector("CurrentActualAmountsDate");
-        fourthItem.innerText = jsondata.publicationStatus;
-        let fifthItem = document.querySelector("PlannedStartDate");
-        fifthItem.innerText = jsondata.publicationStatus;
-        let sixthItem = document.querySelector("PlannedFinishDate");
-        sixthItem.innerText = jsondata.publicationStatus;
-        let sevethItem = document.querySelector("ActualStartDate");
-        seventhItem.innerText = jsondata.publicationStatus;
-        let eigthItem = document.querySelector("ProjectCurrency");
-        eigthItem.innerText = jsondata.publicationStatus;
-        let ninthItem = document.querySelector("ProjectRawActualCost");
-        ninthItem.innerText = jsondata.publicationStatus;
-        let tenthItem = document.querySelector("ProjectBurdenedActualCost");
-        tenthItem.innerText = jsondata.publicationStatus;
-        let eleventhItem = document.querySelector("ProgressStatusCode");
-        eleventhItem.innerText = jsondata.publicationStatus;
-        const ul = document.getElementById("Project-details");
-        projectDetails.innerHTML=""
-        for(const projectData of jsondata.projectDetails){
-            let li = document.createElement("li");
-            li.appendChild(document.createTextNode(projectData));
-            ul.appendChild(li);  
-        }
-      })
-      .catch((error) => console.warn(error));
-  }
-  getProjectData()
-
-  const clearIcon = document.querySelector(".clear-icon");
-  const searchBar = document.querySelector(".search");
-
-  searchBar.addEventListener("keyup", () => {
-    if(searchBar.value && clearIcon.style.visibility != "visible"){
-      clearIcon.style.visibility = "visible";
-    } else if(!searchBar.value) {
-      clearIcon.style.visibility = "hidden";
+  function Search(item){
+    var collection = document.getElementsByClassName("wrapper");
+    for (i = 0;i < collection.length; i++){
+        if (((collection[i].innerHTML).toLowerCase()).indexOf(item) > -1) {
+            collection[i].style.display = "block";
+            } else {
+                collection[i].style.display = "none";
+                }
     }
-  });
-
-  clearIcon.addEventListener("click", () => {
-    searchBar.value = "";
-    clearIcon.style.visibility = "hidden";
-  })
+}
+function projectData() {
+  fetch("https://fierce-forest-94320.herokuapp.com/items")
+    .then((res) => res.json())
+    .then((projectData) => {
+      const ul = document.getElementById("projectDetails");
+      ul.innerHTML = "";
+      for (const item of projectdata) {
+        console.log();
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(item.name));
+        li.id = item.id;
+        li.setAttribute("onclick","beerClick("+item.id+")")
+        ul.appendChild(li);
+      }
+    })
+    .catch((error) => console.warn(error));
+}
