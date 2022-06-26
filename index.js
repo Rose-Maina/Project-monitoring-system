d919ee5c4abc5649c85f7ccc3b585bba3ba1364f
Rose Maina <rosewairimu27@gmail.com>
2022-06-27 00:12:04 +0300

    Add review bar

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 fetch("https://fierce-forest-94320.herokuapp.com/items")
   .then((data) => data.json())
-  .then((completeResearchData) => {
+  .then((completeStadiumData) => {
       //console.log(data1[0]);
       let data1=" "
-      completeResearchData.map((values) => {
+      completeStadiumData.map((values) => {
         data1 = `<div class="researchData">
         <h1 class="ProjectName">${values.ProjectName}</h1>
         <p class="ProgressAsOfDate">${values.ProgressAsOfDate}</p>
@@ -17,28 +17,27 @@ fetch("https://fierce-forest-94320.herokuapp.com/items")
   .then((data) => data.json())
   .then((completeResearchData) => {
       //console.log(data1[0]);
-      let data3=" "
+      let data2=" "
       completeResearchData.map((values) => {
-        data3 = `<div class="researchData">
-        <h1 class="ProjectName">${values.ProjectName[2]}</h1>
-        <p class="ProgressAsOfDate">${values.ProgressAsOfDate[2]}</p>
-        <p class="PublicationStatus">${values.PublicationStatus[2]}</p>
+        data2 = `<div class="researchData">
+        <h1 class="ProjectName">${values.ProjectName[3]}</h1>
+        <p class="ProgressAsOfDate">${values.ProgressAsOfDate[3]}</p>
+        <p class="PublicationStatus">${values.PublicationStatus[3]}</p>
       </div>`
       });
-      document.getElementById("research").innerHTML=data3;
+      document.getElementById("research").innerHTML=data2;
     }
   )
 
-  function Search(item){
-    var collection = document.getElementsByClassName("wrapper");
-    for (i = 0;i < collection.length; i++){
-        if (((collection[i].innerHTML).toLowerCase()).indexOf(item) > -1) {
-            collection[i].style.display = "block";
-            } else {
-                collection[i].style.display = "none";
-                }
-    }
-};
-
-document.getElementById("mybtn").addEventListener("click", e => alert("Sorry, this page is under maintainance!"))
-
+const form =document.getElementById("description-form")
+ form.onsubmit=handleReview
+ function handleReview(event){
+    event.preventDefault()
+    console.log("form submitted")
+    let review = document.getElementById("description").value
+    const ul = document.getElementById("review-list");
+    let li = document.createElement("li");
+        li.appendChild(document.createTextNode(review));
+        ul.appendChild(li);
+    
+ }
