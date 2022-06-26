0245923c544bdedcb196cf70397003a0d7217129
Rose Maina <rosewairimu27@gmail.com>
2022-06-26 23:31:26 +0300

    js changes

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-// let ongoingProjects = () => {
-//   const url = "https://fierce-forest-94320.herokuapp.com/items";
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((projectData) => {
-//         console.log(projectData);
-        
-//         })
-//       .catch((error) => console.log(error));
-//     };
-
-//     let initialize = () => {
-//       ongoingProjects();
-//       projectData(); 
-//     };
-//     initialize();
-
-// function researchInfo() {
-  fetch("https://fierce-forest-94320.herokuapp.com/items")
+fetch("https://fierce-forest-94320.herokuapp.com/items")
   .then((data) => data.json())
   .then((completeResearchData) => {
       //console.log(data1[0]);
-      let data1=+" "
+      let data1=" "
       completeResearchData.map((values) => {
         data1 = `<div class="researchData">
         <h1 class="ProjectName">${values.ProjectName}</h1>
@@ -28,10 +10,25 @@
         <p class="PublicationStatus">${values.PublicationStatus}</p>
       </div>`
       });
-      document.getElementById("research").innerHTML=data1;
+      document.getElementById("stadium").innerHTML=data1;
     }
   )
-    
+fetch("https://fierce-forest-94320.herokuapp.com/items")
+  .then((data) => data.json())
+  .then((completeResearchData) => {
+      //console.log(data1[0]);
+      let data3=" "
+      completeResearchData.map((values) => {
+        data3 = `<div class="researchData">
+        <h1 class="ProjectName">${values.ProjectName[2]}</h1>
+        <p class="ProgressAsOfDate">${values.ProgressAsOfDate[2]}</p>
+        <p class="PublicationStatus">${values.PublicationStatus[2]}</p>
+      </div>`
+      });
+      document.getElementById("research").innerHTML=data3;
+    }
+  )
+
   function Search(item){
     var collection = document.getElementsByClassName("wrapper");
     for (i = 0;i < collection.length; i++){
@@ -42,4 +39,6 @@
                 }
     }
 };
+
+document.getElementById("mybtn").addEventListener("click", e => alert("Sorry, this page is under maintainance!"))
 
