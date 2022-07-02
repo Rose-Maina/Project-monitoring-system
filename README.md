c117b97b2c0ccebc0b1c7c271d4f209204c41c40
Rose Maina <rosewairimu27@gmail.com>
2022-07-02 17:52:51 +0300

    Major Changes

--- README.md
+++ README.md
@@ -17,8 +17,7 @@ https://rose-maina.github.io/Project-monitoring-system/
 # Copyright and License Information
 MIT License
 
-Copyright (c) [2022] [Rose-Maina]
-
+Copyright (c) [2022] [Rose-Maina] [https://fierce-forest-94320.herokuapp.com/items]
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -26,8 +25,7 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-fetch("https://fierce-forest-94320.herokuapp.com/items")
-  .then((data) => data.json())
-  .then((completeStadiumData) => {
-      //console.log(data1[0]);
-      let data1=" "
-      completeStadiumData.map((values) => {
-        data1 = `<div class="researchData">
-        <h1 class="ProjectName">${values.ProjectName}</h1>
-        <p class="ProgressAsOfDate">${values.ProgressAsOfDate}</p>
-        <p class="PublicationStatus">${values.PublicationStatus}</p>
-      </div>`
-      });
-      document.getElementById("stadium").innerHTML=data1;
-    }
-  )
-fetch("https://fierce-forest-94320.herokuapp.com/items")
-  .then((data) => data.json())
-  .then((completeResearchData) => {
-      //console.log(data1[0]);
-      let data2=" "
-      completeResearchData.map((values) => {
-        data2 = `<div class="researchData">
-        <h1 class="ProjectName">${values.ProjectName[3]}</h1>
-        <p class="ProgressAsOfDate">${values.ProgressAsOfDate[3]}</p>
-        <p class="PublicationStatus">${values.PublicationStatus[3]}</p>
-      </div>`
-      });
-      document.getElementById("research").innerHTML=data2;
-    }
-  )
+// document.addEventListener('DOMContentLoaded',() =>{
 
-const icon = document.querySelector('.icon');
-const search = document.querySelector('.search');
-icon.onlick = function() {
-  search.classList.toggle('active');
+let itemOne = document.getElementById("item1")
+let itemTwo = document.getElementById("item2")
+let itemThree = document.getElementById("item3")
+let name = document.getElementById("projectname")
+let picture = document.getElementById("image")
+
+function projectData(){
+  fetch("https://fierce-forest-94320.herokuapp.com/items")
+    .then((data) => data.json())
+    .then((projectData) => {
+        // console.log(projectData);
+        const ul = document.getElementById("projectNames")
+        ul.innerHTML = ""
+        for (const title of projectData){
+          let li = document.createElement("li")
+          li.id = title.id
+          li.onclick = "titleClick()"
+          li.setAttribute("onclick", "titleClick("+title.id+")")
+          li.appendChild(document.createTextNode(title.ProjectName))
+          ul.append(li)
+          // console.log(data.name)
+        }
+    }) 
+}
+projectData()
+    
+function projectItems(title_id){
+  fetch("https://fierce-forest-94320.herokuapp.com/items/" + title_id)
+    .then((response) => response.json())
+    .then((data) => {
+        //console.log(data);
+        name.innerText = data.ProjectName
+        picture.src = data.image   
+        itemOne.innerText = data.ProgressAsOfDate
+        itemTwo.innerText = data.PlannedFinishDate
+        itemThree.innerText = data.ProgressStatusCode
+    })
+}
+projectItems(2)
+
+function titleClick(title_id){
+    projectItems(title_id)
 }
 
-  
-// 
+document.getElementById("myBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Sorry! This page is under maintenance");
+}
+
 const form =document.getElementById("description-form")
  form.onsubmit=handleReview
  function handleReview(event){
     event.preventDefault()
     console.log("form submitted")
     let review = document.getElementById("description").value
-    const ul = document.getElementById("review-list");
+    const ul = document.getElementById("progress");
     let li = document.createElement("li");
         li.appendChild(document.createTextNode(review));
-        ul.appendChild(li);
-    
+        ul.appendChild(li);   
  }
+
+
+document.querySelector(".tabcontent").addEventListener("mouseenter", entering)
+
+  function entering(ev){
+    ev.currentTarget.style.color = "red";
+  }
+ 
